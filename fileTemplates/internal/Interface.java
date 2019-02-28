1245e277277378c1f7a1185d9cb3c201bca75bd8
Sebastian Kramer <skramer@sii.pl.sii.eu>
2019-02-28 22:33:50 +0100

    IU-2018.3.5 <skramer@BVJ2VP2 Create PSI.xml, markdown.xml, IntelliLang.xml, project.default.xml, Obsidian.jar, diff.xml, equalsHashCodeTemplates.xml, Interface.java, go.dep.xml, applicationLibraries.xml, Default.xml, file.template.settings.xml, ignore.xml, find.xml, File Header.java, checkstyle-idea.xml, gradle.run.settings.xml, AndroidComments.xml, code.style.schemes.xml, AndroidLog.xml, My VCS operations.xml, git.xml, mavenVersion.xml, Obsidian.icls, Default.xml, scala.xml, command.xml, laf.xml, q rebase operations.xml, AndroidParcelable.xml, BPMN files.xml, Darcula custom font.icls, Eclipse copy.xml, Kotlin.xml, plain.xml, Kotlin Interface.kt, editor.codeinsight.xml, Android.xml, CORT-formatter.xml, iterations.xml, editor.xml, debugger.xml, path.macros.xml, updates.xml, baseRefactoring.xml, vim_settings.xml, Darcula Big font.icls, ide.general.xml, other.xml, Verimi.xml, plantuml.cfg, rainbow_brackets.xml, lombok-plugin.xml, user.xml, other.xml, keymap.xml, ui.lnf.xml, filetypes.xml, projectView.xml, javaeeExternalResources.xml, remote-servers.xml, plugin_ui.xml, remote-server-client-libraries.xml, colors.scheme.xml, External Tools.xml, notifications.xml, scala_config.xml, Verimi.xml, databaseDrivers.xml, Kotlin Enum.kt, github.xml, templates.xml, stringManipulation.xml, Groovy.xml, consoleFolding.xml, Kotlin Class.kt, goLibraries.xml, go.sdk.xml, parameter.hints.xml, AndroidXML.xml, Class.java, jdk.table.xml, groovy_config.xml, CMake.xml, scala.xml, Default _1_.xml, keyboard.xml, web-browsers.xml, proxy.settings.xml, vcs.xml, databaseSettings.xml, toStringTemplates.xml, ScalaTest Class.scala, usageView.xml

---
+++ fileTemplates/internal/Interface.java
@@ -0,0 +1,12 @@
+//                                                                 
+//  ${NAME}                                               
+//  Copyright ${YEAR} PSI AG. All rights reserved.              
+//  PSI PROPRIETARY/CONFIDENTIAL. Use is subject to license terms
+//                                                                 
+// ******************************************************************
+
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+
+#parse("File Header.java")
+public interface ${NAME} {
+}
